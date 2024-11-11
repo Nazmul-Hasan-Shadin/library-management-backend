78aa89c75d64b806b339d86126a858c22574c11d
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-11-11 17:28:24 +0600

    server ts updated

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import { Server } from "http";
+import dotenv from "dotenv";
 import app from "./app";
 
-
+dotenv.config();
 
 const main = () => {
-  let server: Server = app.listen(3000, () => {
+  let server: Server = app.listen(process.env.PORT, () => {
     console.log("server is listening");
   });
 };
 
-
-main()
+main();
